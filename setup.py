63edb68bf332f5c0451a8e60ee9a994beb05a4c7
estepona <574953550@qq.com>
2019-01-15 19:59:00 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='ut2d',
+      version='0.1',
+      description='a command-line utility to convert unitx timestamp into human readable datetime.',
+      url='https://github.com/estepona/ut2d',
+      author='Binghuan Zhang',
+      author_email='574953550@qq.com',
+      license='MIT',
+      packages=['requests', 'bs4'],
+      zip_safe=False)
